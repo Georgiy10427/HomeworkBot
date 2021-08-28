ded521b54db1bf1cd10fe2b20403c603506dea20
GogaCoder <38399008+Georgiy10427@users.noreply.github.com>
2021-08-28 14:57:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Токен бота из @BotFather
-API_TOKEN = '1726693013:AAFSPp6wpd9cUpYzEfbMnJlyIL_jBxmVYzk'
+API_TOKEN = ''
 # ID владельца бота для возможности администрирования
 owner_id = 1052519447
